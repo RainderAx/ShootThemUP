094beec6408765f0351916df5eb30ced27deb0ec
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-10-17 14:04:13 +0200

    Réorganisation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
-from ship import*
-from ennemis import *
+import random
+import time
+from map import Map
+from vaisseau import Vaisseau
+from ennemi import Ennemi
+from tir import Tir
 
 # Initialisation de Pygame
 pygame.init()
@@ -18,7 +22,30 @@ ROUGE = (255, 0, 0)
 fenetre = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("Shoot Them Up")
 
+# Chargement des images
+vaisseau_img = pygame.image.load("piou.jpg")
+ennemi_img = pygame.image.load("vex.jpg")
+tir_img = pygame.image.load("sphere.png")
+
+# Redimensionnement des images
+vaisseau_img = pygame.transform.scale(vaisseau_img, (64, 64))
+ennemi_img = pygame.transform.scale(ennemi_img, (64, 64))
+tir_img = pygame.transform.scale(tir_img, (32, 32))
+
+# Position initiale du vaisseau
+vaisseau = Vaisseau(largeur // 2 - 32, hauteur - 100, vaisseau_img)
+
+# Liste des ennemis
+ennemis = []
+
+# Gestion des tirs
+tirs = []
+tir_vitesse = 5
+
 score = 0
+
+# Création de la carte
+carte = Map(largeur, hauteur, NOIR)
 
 # Boucle principale du jeu
 running = True
@@ -31,17 +58,67 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                tirer(vaisseau_x + 16, vaisseau_y)
+                Tir.tirer(vaisseau.x + 16, vaisseau.y, tirs, tir_vitesse, tir_img)
+
+    # Déplacement du vaisseau avec les touches directionnelles
+    touches = pygame.key.get_pressed()
+    vaisseau.deplacer(touches, largeur)
+
+    # Mise à jour des ennemis
+    for ennemi in ennemis:
+        ennemi.deplacer()
+
+        # Vérification des collisions entre les ennemis et les tirs
+        for tir in tirs:
+            if ennemi.collision(tir):
+                ennemis.remove(ennemi)
+                tirs.remove(tir)
+                score +=10
+
+        # Vérification des collisions entre les ennemis et le vaisseau
+        if vaisseau.collision(ennemi):
+            running = False
+
+        # Suppression des ennemis qui sont sortis de l'écran
+        if ennemi.y > hauteur:
+            ennemis.remove(ennemi)
+
+    # Mise à jour des tirs
+    for tir in tirs:
+        tir.deplacer()
+
+        # Suppression des tirs qui sont sortis de l'écran
+        if tir.y < 0:
+            tirs.remove(tir)
+
+    # Création d'un ennemi à intervalles réguliers
+    if random.randint(0, 100) < 2:
+        if random.randint(0, 1) < 1:
+            ennemi = Ennemi(random.randint(0, largeur - 64), -64, vaisseau_img, 1)
+            ennemis.append(ennemi)
+        else:
+            ennemi_rapide = Ennemi(random.randint(0, largeur - 64), -64, ennemi_img, 1)
+            ennemi_rapide.vit *= 2
+            ennemis.append(ennemi_rapide)
 
 
+    # Difficulté en fonction du score
+    for ennemi in ennemis:
+        Diff = (score + 200)/100
+        Diff2= Diff + 1
+        ennemi.vit = random.randint(int(Diff), int(Diff2))
+        if Diff > 900:
+            if random.randint(0, 100) < 2:
+                ennemi = Ennemi(random.randint(0, largeur - 64), -64, ennemi_img, random.randint(1, 3))
+                ennemis.append(ennemi)
 
     # Dessin des éléments du jeu
-    fenetre.fill(NOIR)
-    fenetre.blit(vaisseau_img, (vaisseau_x, vaisseau_y))
+    carte.dessiner(fenetre)
+    vaisseau.afficher(fenetre)
     for ennemi in ennemis:
-        fenetre.blit(ennemi_img, (ennemi["x"], ennemi["y"]))
+        ennemi.afficher(fenetre)
     for tir in tirs:
-        fenetre.blit(tir_img, (tir["x"], tir["y"]))
+        tir.afficher(fenetre)
 
     #affichage du score 
     font = pygame.font.Font(None, 36)
@@ -55,4 +132,4 @@ while running:
     clock.tick(60)
 
 # Fermeture de Pygame
-pygame.quit()
+pygame.quit()  
--- tir.py
+++ tir.py
@@ -1,36 +1,17 @@
-from ship import Vaisseau 
 import pygame
 
-
-tir_img = pygame.image.load("sphere.png")
-tir_img = pygame.transform.scale(tir_img, (32, 32))
-
-
 class Tir:
-    def __init__(self, x, y):
-        self.image = tir_img
+    def __init__(self, x, y, vit, img):
         self.x = x
         self.y = y
-        self.vitesse = 5
+        self.vit = vit
+        self.img = img
 
     def deplacer(self):
-        self.y -= self.vitesse
+        self.y -= self.vit
 
-class Jeu:
-    def __init__(self, largeur, hauteur):
-        self.vaisseau = Vaisseau(largeur, hauteur)
-        self.tirs = []
-
-    def tirer(self):
-        tir = Tir(self.vaisseau.x, self.vaisseau.y)
-        self.tirs.append(tir)
-
-    def mise_a_jour_tirs(self):
-        tirs_a_supprimer = []
-        for tir in self.tirs:
-            tir.deplacer()
-            if tir.y < 0:
-                tirs_a_supprimer.append(tir)
-
-        for tir in tirs_a_supprimer:
-            self.tirs.remove(tir)
+    def afficher(self, fenetre):
+        fenetre.blit(self.img, (self.x, self.y))
+    def tirer(x, y, tirs, tir_vitesse, tir_img):
+        tir = Tir(x, y, tir_vitesse, tir_img)
+        tirs.append(tir)
